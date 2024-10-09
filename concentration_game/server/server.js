1069be32f36f026481ce7541b0106bbfe03e78d8
Anitejjain <131655465+Anitejjain@users.noreply.github.com>
2024-10-09 22:20:09 +0530

    Update server.js
    Included game route in the main server file

--- concentration_game/server/server.js
+++ concentration_game/server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const game = require('./routes/game');
 const db = require('./db');
 
 const app = express();
@@ -11,8 +13,8 @@ app.use(bodyParser.json());
 
 db.connect();
 
-
 app.use('/api', authRoutes);
+app.use('/api/game',game);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
